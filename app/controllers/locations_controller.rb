2bf753c6e3f01a8b54f997154c45d408160c8817
John Harlow <info@jharlow.uk>
2024-08-08 15:28:33 -0700

    feat(locations): can edit locations!

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,7 @@ class LocationsController < ApplicationController
   before_action :set_locatable
 
   def show
+    logger.info(params)
     @location = Location.find(params[:id])
   end
 
@@ -12,9 +13,22 @@ class LocationsController < ApplicationController
   def create
     @location = @locatable.locations.new(location_params)
     if @location.save
-      redirect_to @locatable.path
+      redirect_to(@locatable.path)
     else
-      render :new, status: :unprocessable_entity
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to(@locatable.path)
+    else
+      render(:edit, status: :unprocessable_entity)
     end
   end
 
