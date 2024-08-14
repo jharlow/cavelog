7db529a482cc87cad5af5c983fa39f1f6eb9af2d
John Harlow <info@jharlow.uk>
2024-08-14 09:16:59 -0700

    feat(breadcrumbs): added breadcrumbs to pages and improved overflow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def remove_page_padding(&block)
-    content_tag(:div, class: "-mx-4 sticky top-12 py-2 px-6 w-[calc(100% + 2rem)] bg-white shadow-sm z-40") do
+  def page_padding(&block)
+    content_tag(:div, class: "flex flex-col gap-4 p-4") do
       capture(&block)
     end
   end
