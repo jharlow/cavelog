e6cca115b00e00f5f0f846e1286f36bf9dd43e28
John Harlow <info@jharlow.uk>
2024-08-13 21:50:12 -0700

    feat(breadcrumbs): added first breadcrumb trail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def remove_page_padding(&block)
+    content_tag(:div, class: "-mx-4 sticky top-12 py-2 px-6 w-[calc(100% + 2rem)] bg-white shadow-sm z-40") do
+      capture(&block)
+    end
+  end
 end
